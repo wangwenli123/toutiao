64581660711a54c6919656846fe30ef70142cbd7
wangwenli <2455287350@qq.com>
2022-12-12 19:43:04 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'//引入组件 还要注册
+import './plugins/element.js'
+
+import './styles/index.less'
+
+
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);//注册需要的组件
 
 new Vue({
   router,
---
+++ src/plugins/element.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import "element-ui/lib/theme-chalk/index"
+import { Button, Form, FormItem, Input } from 'element-ui'
+
+Vue.use(Button);
+Vue.user(Form);
+Vue.user(FormItem);
+Vue.user(Input);
+
